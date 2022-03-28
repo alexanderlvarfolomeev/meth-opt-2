ae85fb719260ca94121d90f9608c5ab1ec10ce75
Varfolomeev Alexander <vas.aleks.v@gmail.com>
2022-03-29 00:41:36 +0300

    GD Modifications

--- gradient.py
+++ gradient.py
@@ -1,5 +1,6 @@
 import random
 from typing import Tuple, Any
+from abc import abstractmethod
 
 import numpy as np
 from numpy import ndarray
@@ -9,6 +10,98 @@ from graphic import Graphic, Linear
 from loss import Loss
 
 
+class Gradient:
+    @abstractmethod
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        pass
+
+    @abstractmethod
+    def get_memory_cost(self, batch_size: int) -> int:
+        pass
+
+
+class NaiveGradient(Gradient):
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        return loss.loss_gradient_points(Linear(w), points_x, points_y)
+
+    def get_memory_cost(self, batch_size: int) -> int:
+        return 0
+
+
+class Momentum(Gradient):
+    def __init__(self, n: int, b: float):
+        self.b = b
+        self.previous_gradient = np.zeros(n)
+
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        prev = self.previous_gradient
+        self.previous_gradient = loss.loss_gradient_points(Linear(w), points_x, points_y)
+        return self.b * prev + (1 - self.b) * self.previous_gradient
+
+    def get_memory_cost(self, batch_size: int) -> int:
+        return self.previous_gradient.size * 2
+
+
+class Nesterov(Gradient):
+    def __init__(self, n: int, b: float):
+        self.b = b
+        self.previous_gradient = np.zeros(n)
+
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        shift = self.b * self.previous_gradient
+        shifted_points_x = points_x - shift
+        self.previous_gradient = loss.loss_gradient_points(Linear(w), shifted_points_x, points_y)
+        return shift + (1 - self.b) * self.previous_gradient
+
+    def get_memory_cost(self, batch_size: int) -> int:
+        return self.previous_gradient.size * (batch_size + 2)
+
+
+class AdaGrad(Gradient):
+    def __init__(self, n: int, b: float):
+        self.b = b
+        self.gradient_accumulator = np.zeros(n)
+
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        grad = loss.loss_gradient_points(Linear(w), points_x, points_y)
+        self.gradient_accumulator += grad ** 2
+        return grad / np.sqrt(self.gradient_accumulator + 1e-8)
+
+    def get_memory_cost(self, batch_size: int) -> int:
+        return self.gradient_accumulator.size * 2
+
+
+class RMSProp(Gradient):
+    def __init__(self, n: int, g: float):
+        self.g = g
+        self.gradient_accumulator = np.zeros(n)
+
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        grad = loss.loss_gradient_points(Linear(w), points_x, points_y)
+        self.gradient_accumulator = self.g * self.gradient_accumulator + (1 - self.g) * grad ** 2
+        return grad / np.sqrt(self.gradient_accumulator + 1e-8)
+
+    def get_memory_cost(self, batch_size: int) -> int:
+        return self.gradient_accumulator.size * 3
+
+
+class Adam(Gradient):
+    def __init__(self, n: int, b: float, g: float):
+        self.b = b
+        self.previous_gradient = np.zeros(n)
+        self.g = g
+        self.gradient_accumulator = np.zeros(n)
+
+    def compute_weights_diff(self, points_x: ndarray, points_y: ndarray, w: ndarray, loss: Loss) -> ndarray:
+        grad = loss.loss_gradient_points(Linear(w), points_x, points_y)
+        self.previous_gradient = self.b * self.previous_gradient + (1 - self.b) * self.previous_gradient
+        self.gradient_accumulator = self.g * self.gradient_accumulator + (1 - self.g) * grad ** 2
+        return self.previous_gradient / np.sqrt(self.gradient_accumulator + 1e-8)
+
+    def get_memory_cost(self, batch_size: int) -> int:
+        return self.gradient_accumulator.size * 5
+
+
 def gradient(graphic: Graphic,
              learning_rate: float,
              w: ndarray,
@@ -16,7 +109,8 @@ def gradient(graphic: Graphic,
              epoches: int,
              batch_size: int,
              criteria: StopCriteria,
-             eps: float
+             eps: float,
+             grad: Gradient = NaiveGradient()
              ) -> Tuple[int, Any]:
     batch_number = (graphic.points_x.shape[0] + batch_size - 1) // batch_size
     for epoch in range(epoches):
@@ -29,8 +123,7 @@ def gradient(graphic: Graphic,
                        range(batch_index * batch_size, min(graphic.points_x.shape[0], (batch_index + 1) * batch_size))]
             points_x = np.array([graphic.points_x[pos] for pos in indexes])
             points_y = np.array([graphic.points_y[pos] for pos in indexes])
-            new_empiric_risk = loss.loss_gradient_points(Linear(w), points_x, points_y)
-            w = w - learning_rate * new_empiric_risk
+            w = w - learning_rate * grad.compute_weights_diff(points_x, points_y, w, loss)
             if criteria.stop(w, loss, points_x, points_y, eps):
                 return epoch, w
     return epoches, w
