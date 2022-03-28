4d1a218ccf90ec1b13086d0c436f9fea892ec96e
Vladislav Kuznetsov <Vladislav.Kuznetsov@jetbrains.com>
2022-03-29 00:36:44 +0300

    Fix

--- graphic.py
+++ graphic.py
@@ -17,9 +17,10 @@ class Linear:
 
 class Graphic:
 
-    def __init__(self, linear: Linear, start: float, finish: float, noise_level: float, count: int):
+    def __init__(self, linear: Linear, start: float, finish: float, noise_level: float, count: int, seed: int):
         self.linear = linear
         dist = finish - start
+        np.random.seed(seed)
         temp = np.random.rand(count, linear.n) * dist + start
         self.points_x = temp
         self.points_y = np.array(
@@ -36,9 +37,20 @@ class Graphic:
 
 class F(Graphic):
     def __init__(self):
-        super().__init__(Linear(np.array([1, 2])), -1, 1, 0.01, 50)
+        super().__init__(Linear(np.array([1, 2])), -1, 1, 0.01, 50, 121391674)
 
 
 class G(Graphic):
     def __init__(self):
-        super().__init__(Linear(np.array([3.5, 6])), -10, 10, 1.5, 50)
+        super().__init__(Linear(np.array([3.5, 6])), -10, 10, 1.5, 50, 386484234)
+
+
+class F3d(Graphic):
+    def __init__(self):
+        super().__init__(Linear(np.array([1, 1, 2])), -1, 1, 0.01, 100, 28347834)
+
+
+class F10d(Graphic):
+    def __init__(self):
+        super(F10d, self).__init__(Linear(
+            np.array([1.1, 34, 2.1, 0.2343, 34.4, -1.3, 1, 12.2, 0.01, -0.01])), -100, 100, 1.5, 100, 364937374)
